10afd3c75f47fce1a5223daa3de31064528f3c9e
myra-sehgal <bhavna.khera16@gmail.com>
2025-07-08 11:47:14 +0400

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,28 @@
+name = " myra"
+age = 12
+is_student = True 
+weight = 38.5
+
+print ( "name : " , (name))
+print ("datatype of name is:",type(name))
+
+print ( "age : " , (age))
+print ("datatype of age:",type(age))
+
+print ( " is_student  : " , (is_student))
+print ("datatype of is_student:",type(is_student))
+
+print ( "weight : " , (weight))
+print ("datatype of weight:",type(weight))
+
+# type casting to change the data type of the variables 
+
+print ( "\nafter type casting .... ")
+
+age = str(age)
+print("age=",(age))
+print ("datatype of age:",type(age))
+
+weight = int(weight)
+print("weight=" , (weight))
+print ("datatype of weight:",type(weight))
