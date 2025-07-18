af7d50f35b6a1b42f29a30f3e8a6a9917b15fb78
myra-sehgal <bhavna.khera16@gmail.com>
2025-07-18 15:59:54 +0400

    Add files via upload

---
+++ storing birthdays.py
@@ -0,0 +1,11 @@
+name=str(input("please choose beetween myra, meher , kiara and nivaan to find out their birthday-"))
+if name=="myra":
+    print("25th august 2013")
+elif name=="meher":
+    print("27th december 2016")
+elif name=="kiara":
+    print("9th january 2019") 
+elif name=="nivaan":
+    print("7th december 2021") 
+else:
+    print("that name was not an option!!!")
