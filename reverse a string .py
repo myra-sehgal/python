9ec4ddb07f88b8b8681a83a9806974c8e8998820
myra-sehgal <bhavna.khera16@gmail.com>
2025-07-24 11:48:14 +0400

    Add files via upload

---
+++ reverse a string .py
@@ -0,0 +1,6 @@
+string=(input("please enter the string"))
+string2=(' ')
+for i in string:
+    string2=i+string2
+print("\n the original string=",string)
+print("\n the reversed string=",string2)
---
+++ sum of whole numbers.py
@@ -0,0 +1,5 @@
+n = int(input("please enter the whole number whose sum you want to find:"))
+sum = 0
+for i in range(1,n+1):
+    sum = sum +1
+    print ("\n sum =",sum)
