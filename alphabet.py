ef07ffc21c37e83b47d91aeb51390226157bc18e
myra-sehgal <bhavna.khera16@gmail.com>
2025-07-27 21:25:52 +0400

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+alp=str(input("please enter a letter , symbol or number-"))
+if alp=="a" or "b" or "c" or "d" or "e" or "f" or "g" or "h"or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z":
+    print("this is an alphabet")
+else:
+    print("this is not an alpahbet")
