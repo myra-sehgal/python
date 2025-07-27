bcb1893030b3d7276b7b136cfc0219b15a2a55fe
myra-sehgal <bhavna.khera16@gmail.com>
2025-07-27 21:17:56 +0400

    Add files via upload

---
+++ weather clothing.py
@@ -0,0 +1,7 @@
+weather=int(input("please enter the weather right now-"))
+if weather > 40:
+    print("wear light clothes")
+elif weather >30:
+    print("wear not to light clothes")
+else:
+    ("wear heavy but not to heavy clothing")    
