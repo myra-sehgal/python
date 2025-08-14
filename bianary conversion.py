39a135aaf7c0df4f6cb5b4b34a24efdd7df74821
myra-sehgal <bhavna.khera16@gmail.com>
2025-08-14 11:02:38 +0400

    Add files via upload

---
+++ bianary conversion.py
@@ -0,0 +1,4 @@
+# Convert decimal to binary using bin()
+decimal_number=float(input("please enter a decimal number"))
+binary_number = bin(decimal_number) 
+print(f"Binary conversion of {decimal_number} is {binary_number}")
