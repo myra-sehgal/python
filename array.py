e47a3bcb274bf0baf3666b33be84736982d74592
myra-sehgal <bhavna.khera16@gmail.com>
2025-08-20 11:52:00 +0400

    Add files via upload

---
+++ array.py
@@ -0,0 +1,7 @@
+import array as arr
+array_num = arr.array('i'(1,2,3,5,7,9,5,111,7,4,5,2,4,7,))
+print('original array:'+str(array_num))
+print("the number 7 has occured in the said array="+str(array_num.count(7)))
+array_num.reverse()
+print('the reversed oder of the given array:')
+print(str(array_num))
---
+++ set intersection.py
@@ -0,0 +1,8 @@
+setx={'pink','yellow'}
+setz={'pink','purple'}
+print('original sets:')
+print(setx)
+print(setz)
+print("intersection of the two said sets:")
+sety=setx.intersection(setz)
+print(sety)
